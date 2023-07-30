fb81565df95464a1a1d40378f5a2c4a0c8ff3343
deivis8AM <deivisochoamrz@gmail.com>
2023-07-30 11:25:28 -0500

    Terminando Express

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -1,30 +1,82 @@
-const axios = require("axios")
-const URL = "https://rickandmortyapi.com/api/character/"
+const axios = require('axios'); // exportamos a axios
+const URL = 'https://rickandmortyapi.com/api/character/';
+// const { response } = require('express');
 
-const getCharById = (res, id) => {            
-    axios(URL + id)   
-    // hacemos una promesa 
-    .then((response) => response.data)  // nos devuelve otra promesa 
-    .then((data) =>{
+
+const getCharById = (req, res) => {
+  const {id} = req.params;
+
+     axios(URL + id)    // hacemos la peticion a la app 
+     .then((response) => response.data)
+     .then(( data) => {     // Hacemos una promesa 
         const character = {
-            id: data.id,
-            name: data.name,
-            gender: data.gender,
-            species: data.species,
-            origin: data.origin?.name,
-            Image: data.Image,
-            status: data.status
-        }
-        res.writeHead(200, {"Content-type":"application/json"}) // Respuesta en en formato JSON y status 200
-        res.end(JSON.stringify(character)) // Respuesta final con el personaje requerido 
-    })
-    .catch((error) => {      // Un catch para manejar el error siempre al final
-        res.writeHead(500, {"Content-type":"text/plain"})
-        res.end(error.message)
-        // res.writeHead(500, {"Content-type":"applicationa/json"}) // Utilizamos este tipo de contenido para enviar un objeto como respuesta final
-        // res.end({error: error.message})
-    })
-}
+          id: data.id,
+          name: data.name,
+          gender: data.gender,
+          species: data.species, 
+          origin: data.origin?.name, 
+          image: data.image,
+         status: data.status,
+       };
+        
+       character.name
+        ? res.status(200).json(character)
+        : res.status(404).send("Not found")
+     })
+     .catch((error) => {     // Atrapamos el error con catch
+         res.status(500).json({message: error.message}) // en viamos el error 
+     });
+};
+
+// exportamos a getCharById
+module.exports = getCharById;
 
 
-module.exports =getCharById // exportamos 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const axios = require("axios")
+// const URL = "https://rickandmortyapi.com/api/character/"
+
+// const getCharById = (res, id) => {            
+//     axios(URL + id)   
+//     // hacemos una promesa 
+//     .then((response) => response.data)  // nos devuelve otra promesa 
+//     .then((data) =>{
+//         const character = {
+//             id: data.id,
+//             name: data.name,
+//             gender: data.gender,
+//             species: data.species,
+//             origin: data.origin?.name,
+//             Image: data.Image,
+//             status: data.status
+//         }
+//         res.writeHead(200, {"Content-type":"application/json"}) // Respuesta en en formato JSON y status 200
+//         res.end(JSON.stringify(character)) // Respuesta final con el personaje requerido 
+//     })
+//     .catch((error) => {      // Un catch para manejar el error siempre al final
+//         res.writeHead(500, {"Content-type":"text/plain"})
+//         res.end(error.message)
+//         // res.writeHead(500, {"Content-type":"applicationa/json"}) // Utilizamos este tipo de contenido para enviar un objeto como respuesta final
+//         // res.end({error: error.message})
+//     })
+// }
+
+
+// module.exports =getCharById // exportamos 
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,15 +1,29 @@
-const http = require("http") // Generar mi servido 
-const getCharById = require("./controllers/getCharById")   // Importamos el getCharById
+const express = require("express");
+const router = require("./routes");
+const server = express();
+const PORT = 3001;
 
-http.createServer((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // permite el acceso al fron para que puedad acceder y levantarlo desde el navegador
-// Creamos un condicional 
-if(req.url.includes("/rickandmorty/character")) {
-    let id = req.url.split("/").at(-1)
-    getCharById(res, id)
- }
+server.listen(PORT, () =>{
+   console.log("Server raised in port: " + PORT);
+});
 
-}).listen(3001, () => console.log("port on 3001"))
+server.use((req, res, next) => {
+       res.header('Access-Control-Allow-Origin', '*');
+       res.header('Access-Control-Allow-Credentials', 'true');
+       res.header(
+          'Access-Control-Allow-Headers',
+          'Origin, X-Requested-With, Content-Type, Accept'
+       );
+       res.header(
+          'Access-Control-Allow-Methods',
+          'GET, POST, OPTIONS, PUT, DELETE'
+       );
+       next();
+    });
+
+server.use(express.json()); // transforma los archivos json
+
+server.use("/rickandmorty",router);   //Crea un middleware que agregue el string "/rickandmorty" antes de cada una de tus rutas.
 
 
 
@@ -17,6 +31,78 @@ if(req.url.includes("/rickandmorty/character")) {
 
 
 
+
+
+
+
+// const express = require('express')   // importamos express
+// const server = express() // Ejecutamos a express
+// const router = require("./routes") // importamos a router
+// const PORT = 3001
+// const cors = require('cors')
+
+// server.use(cors())
+
+
+
+// server.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header(
+//        'Access-Control-Allow-Headers',
+//        'Origin, X-Requested-With, Content-Type, Accept'
+//     );
+//     res.header(
+//        'Access-Control-Allow-Methods',
+//        'GET, POST, OPTIONS, PUT, DELETE'
+//     );
+//     next();
+//  });
+
+
+//  server.use(express.json()); 
+//  server.use('/rickandmorty', router)
+
+// server.listen(PORT, () => console.log('Server raised in port: ' + PORT)) // Ejecutamos el metodo listen-->(escuchar) para que escuche nuestro puerto
+// //Acabamos de crear el servidor con express
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const http = require("http") // Generar mi servido 
+// const getCharById = require("./controllers/getCharById")   // Importamos el getCharById
+
+// http.createServer((req, res) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*'); // permite el acceso al fron para que puedad acceder y levantarlo desde el navegador
+// // Creamos un condicional 
+// if(req.url.includes("/rickandmorty/character")) {
+//     let id = req.url.split("/").at(-1)
+//     getCharById(res, id)
+//  }
+
+// }).listen(3001, () => console.log("port on 3001"))
+
+
+
+//--------------------------------------------------------------------
 
 
 
